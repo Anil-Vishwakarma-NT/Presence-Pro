aff9d5c32acb3514b593a37bb273ad20a35cc364
Anil Vishwakarma <anil.vishwakarma@nucleusteq.com>
2025-01-24 15:13:26 +0530

    first commit of config

---
+++ config/config.py
@@ -0,0 +1,31 @@
+# import os
+
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# UPLOAD_FOLDER = os.path.join(BASE_DIR, "../data/Upload Here")
+# PROCESSED_FILES_FOLDER = os.path.join(BASE_DIR, "../data/Processed Files")
+
+# SUBFOLDERS = {
+#     'One Keka Excel File': os.path.join(UPLOAD_FOLDER, 'One Keka Excel File'),
+#     'One Indore Biometric Excel File': os.path.join(UPLOAD_FOLDER, 'One Indore Biometric Excel File'),
+#     'One Raipur Biometric Excel File': os.path.join(UPLOAD_FOLDER, 'One Raipur Biometric Excel File'),
+# }
+
+# CONSOLIDATED_FILE = 'consolidated_attendance_optimized.xlsx'
+
+
+
+
+
+UPLOAD_FOLDER = 'Upload Here'
+PROCESSED_FILES_FOLDER = 'Processed Files'
+CONSOLIDATED_FILE_PATH = 'consolidated_attendance_optimized.xlsx'
+
+# Define subfolder paths within 'Upload Here'
+SUBFOLDERS_FOR_UPLOAD_FOLDERS = {
+    'One Keka Excel File': None,
+    'One Indore Biometric Excel File': None,
+    'One Raipur Biometric Excel File': None
+}
+
+
---
+++ config/notification_config.py
@@ -0,0 +1,19 @@
+# [Paths]
+AttendanceData = "Processed Files/consolidated_attendance_optimized.xlsx"
+EmployeeEmailData = "email_detials/employees_email_ID.xlsx"
+
+
+# [Email]
+SMTPServer = "smtp.gmail.com"
+SMTPPort = "587"
+SenderEmail = "anilvishwakarma4494@gmail.com"
+SenderPassword = "swup wnrk vdnu qcng"
+EmailSubject = "Absence Notification"
+EmailBodyTemplate = "src/templates/email_body.txt"
+
+# [Application]
+SkipRowsEmailFile = "2"
+
+
+#  C:\Users\DELL\Desktop\PRESENCE PRO 2.O\email_detials\employees_email_ID.xlsx
+#  C:\Users\DELL\Desktop\PRESENCE PRO 2.O\src\templates\email_body.txt
